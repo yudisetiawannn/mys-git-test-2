d34d66f59209f797160654aadf1bab0d8f42506f
yudisetiawannn <mhmmdyudistwn20@gamil.com>
2024-05-11 18:45:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, aspernatur dolorem neque modi possimus magni tenetur, eius at nisi consequuntur veniam soluta illo recusandae culpa eligendi labore eos beatae id?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
